3ec81a994dc0db72399e7c8b9f99ea57897440d9
tomirios2005 <tomirios2005@gmail.com>
2025-11-12 15:01:18 -0300

    asd

--- admin-services/src/main/java/tpe/microservicios/admin_service/service/dto/response/TarifaResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package tpe.microservicios.admin_service.service.dto.response;
-
-public record TarifaResponseDTO(
-        Long id,
-        String tipo,
-        Float valor,
-        Integer pausaMaxMinutos
-) {}
